a2d53047e76b0314f091d1a8658b140ebcf7cbe7
NegativeAgar <57542352+NegativeAgar@users.noreply.github.com>
2020-01-26 13:52:36 +0200

    Add files via upload

--- role.py
+++ role.py
@@ -2,11 +2,13 @@
 from datetime import date
 import discord
 from discord.ext import commands
+import time
+import config
 import os
-from discord.ext.commands import Bot
-import asyncio
-#API_KEY = "AIzaSyC3qBObRiGZaV1icDbcN3HS7gcbpose6Uk"
 
+#API_KEY = "AIzaSyC3qBObRiGZaV1icDbcN3HS7gcbpose6Uk"
+named_tuple = time.localtime()
+time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
 
 prefix = "."
 bot = commands.Bot(command_prefix= prefix)
@@ -26,7 +28,7 @@ bot.remove_command('help')
 
 @bot.command()
 async  def help(ctx):
-    emb = discord.Embed(title= "Помощь")
+    emb = discord.Embed(title= "Помощь",colour= discord.Colour.orange())
 
     emb.add_field(name="{}команды".format(prefix), value="Список команд")
 
@@ -36,15 +38,18 @@ async  def help(ctx):
 @commands.has_permissions(administrator = True)
 async  def ahelp(ctx):
 
-    emb = discord.Embed(title= "Админ команды:", colour= discord.Colour.red())
+    emb = discord.Embed(title= "Админ команды:", colour= discord.Colour.orange())
 
     await ctx.channel.purge(limit=10)
 
-    emb.add_field(name="{}clear".format(prefix), value="Очистить чат",inline=False)
-    emb.add_field(name="{}ainfo (ID пользователя)".format(prefix), value="Информация про пользователя",inline=False)
-    emb.add_field(name="{}ban (ID пользователя)".format(prefix), value="Забанить",inline=False)
-    emb.add_field(name="{}kick (ID пользователя)".format(prefix), value="Кикинуть",inline=False)
-    emb.add_field(name="{}mute (ID пользователя)".format(prefix), value="Дать мут",inline=False)
+    emb.add_field(name="Очистить чат", value="{}clear".format(prefix),inline=False)
+    emb.add_field(name="Информация про пользователя", value="{}ainfo (ID пользователя)".format(prefix),inline=False)
+    emb.add_field(name="Забанить", value="{}ban [ID пользователя] [причина бана]".format(prefix),inline=False)
+    emb.add_field(name="Разбанить", value="{}unban [ID пользователя]".format(prefix),inline=False)
+    emb.add_field(name="Кикинуть", value="{}kick [ID пользователя] [причина кика]".format(prefix),inline=False)
+    emb.add_field(name="Дать мут", value="{}mute [ID пользователя] [причина мута]".format(prefix),inline=False)
+    emb.add_field(name="Снять мут", value="{}unmute [ID пользователя]".format(prefix),inline=False)
+    emb.add_field(name="Админ команды:", value="{}ahelp".format(prefix),inline=False)
 
     await ctx.send(embed=emb)
 # clear
@@ -63,10 +68,11 @@ async def clear(ctx, count=100):
 # Чат бот
 @bot.command()
 async def руха(ctx,arg):
-    if arg in cn:
         author = ctx.message.author
-        await ctx.send(f"Вот держи братан {author.mention}!\nhttps://www.youtube.com/channel/UC7uCfzRfy2UDVtuxnr3jjBA/videos")
-
+        if arg in cn:
+            await ctx.send(f"Вот держи братан {author.mention}!\nhttps://www.youtube.com/channel/UC7uCfzRfy2UDVtuxnr3jjBA/videos")
+        else:
+            await  ctx.send(f'Я тебя не понимаю!{author.mention}')
 
 # отключение от канала
 @bot.event
@@ -86,20 +92,24 @@ async def on_member_join(member: discord.Member):
 # commands
 @bot.command()
 async def команды(ctx):
-    emb = discord.Embed(title= "Команды сервера", colour= discord.Colour.red())
+    emb = discord.Embed(title= "Команды ботов", colour= discord.Colour.orange())
     await ctx.channel.purge(limit=10)
-    emb.add_field(name= '{}info'.format(prefix), value='Информация о пользователе.',inline=False)
+    emb.add_field(name= 'Информация о пользователе.', value='{}info'.format(prefix),inline=False)
+    emb.add_field(name='Поговорить с ботом.', value='.руха [text]', inline=False)
     await ctx.send(embed= emb)
 # mute
 @bot.command()
 @commands.has_permissions(administrator = True)
 async def mute(ctx, member: discord.Member, *,reason=None):
-    emb = discord.Embed(title="Выдан мут пользователю", colour=discord.Colour.red())
-    ctx.channel.purge(limit=1)
+    emb = discord.Embed(title="Выдан мут пользователю!", colour=discord.Colour.red())
+    await ctx.channel.purge(limit=1)
     role = discord.utils.get(member.guild.roles, id=670630763716149248)
     await member.add_roles(role)
     emb.set_author(name= member.name, icon_url=member.avatar_url)
-    emb.add_field(name='Пользователь замучен!', value='Замутен {} с причиной {}'.format(member.name,format(reason)))
+    emb.add_field(name='Имя:', value=member.name)
+    emb.add_field(name='ID пользователя:', value=member.id)
+    emb.add_field(name='Причина:', value=reason, inline=False)
+    emb.add_field(name='Время:', value=time_string,inline=False)
     emb.set_footer(text= 'Замучен Администратором {}'.format(ctx.author.name), icon_url =ctx.author.avatar_url)
     await ctx.send(embed=emb)
 
@@ -108,35 +118,57 @@ async def mute(ctx, member: discord.Member, *,reason=None):
 @commands.has_permissions(administrator = True)
 async def unmute(ctx, member: discord.Member):
     emb = discord.Embed(title="Пользователь размучен!", colour=discord.Colour.green())
-    ctx.channel.purge(limit=1)
+    await ctx.channel.purge(limit=1)
     role = discord.utils.get(member.guild.roles, id=670630763716149248)
     await member.remove_roles(role)
     emb.set_author(name= member.name, icon_url=member.avatar_url)
     emb.set_footer(text= 'Мут снят Администратором {}'.format(ctx.author.name), icon_url =ctx.author.avatar_url)
     await ctx.send(embed=emb)
-
+# ban
 @bot.command()
 @commands.has_permissions(administrator = True)
 async def ban(ctx, member: discord.Member, *,reason=None):
         emb = discord.Embed(title="Пользователь забанен", colour=discord.Colour.red())
-        ctx.channel.purge(limit=1)
+        await ctx.channel.purge(limit=1)
         await member.ban(reason=reason)
         emb.set_author(name= member.name, icon_url=member.avatar_url)
-        emb.add_field(name='', value='Забанен {} с причиной {}'.format(member.name,format(reason)))
+        emb.add_field(name='Имя:', value=member.name)
+        emb.add_field(name='ID пользователя:', value=member.id)
+        emb.add_field(name='Причина:', value=reason,inline=False)
+        emb.add_field(name='Время:', value=time_string)
         emb.set_footer(text= 'Забанен Администратором {}'.format(ctx.author.name), icon_url =ctx.author.avatar_url)
         await ctx.send(embed=emb)
 
+#unban
+@bot.command()
+@commands.has_permissions(administrator = True)
+async def unban(ctx):
+    ctx.send('Команда в разработке!')
+
+#kick
+@bot.command()
+@commands.has_permissions(administrator = True)
+async def kick(ctx, member: discord.Member,reason=None):
+    emb = discord.Embed(title='Пользаватель кикнут!', colour=discord.Colour.red())
+    await ctx.channel.purge(limit=1)
+    emb.set_author(name= member.name, icon_url=member.avatar_url)
+    emb.add_field(name='Имя::', value=member.name)
+    await member.kick(reason=reason)
+    emb.add_field(name='ID пользователя:', value=member.id)
+    emb.add_field(name='Причина:', value=reason,inline=False)
+    emb.add_field(name='Время:', value=time_string)
+    emb.set_footer(text= 'Кикнут Администратором {}'.format(ctx.author.name), icon_url =ctx.author.avatar_url)
+    await ctx.send(embed=emb)
 #info
 @bot.command()
-async def info(ctx, user: discord.User):
-        emb = discord.Embed(title="Статистика пользователя {}".format(user.name), colour=discord.Colour.red())
-        ctx.channel.purge(limit=1)
-        emb.set_author(name= user.name, icon_url=user.avatar_url)
-        emb.add_field(name=user.name, value='Имя пользователя')
-        #emb.add_field(name="Создание профиль дискорда: ", value=str(user.created_at)[:16])
-        emb.set_footer(text= 'Смотрит статистику {}'.format(ctx.author.name), icon_url =ctx.author.avatar_url)
-        await ctx.send(embed=emb)
+async def info(ctx,user: discord.User):
+    emb = discord.Embed(title="Статистика пользователя {}".format(user.name), colour=discord.Colour.blue())
+    await ctx.channel.purge(limit=1)
+    emb.set_author(name= user.name, icon_url=user.avatar_url)
+    emb.add_field(name='Имя пользователя:', value=user.name)
+    emb.add_field(name="Создание профиля дискорда: ", value=str(user.created_at)[:16])
+    emb.add_field(name='ID пользователя:', value=user.id,inline=False)
+    emb.set_footer(text= 'Смотрит статистику {}'.format(ctx.author.name), icon_url =ctx.author.avatar_url)
+    await ctx.send(embed=emb)
 
-
-client = os.environ.get('BOT_TOKEN')
-bot.run(str(token))
+bot.run(config.token)

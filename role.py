22e1a15c8c7ba1acfebe40fbee7c8fcb464f68a1
NegativeAgar <57542352+NegativeAgar@users.noreply.github.com>
2020-02-07 12:23:15 +0200

    Update role.py

--- role.py
+++ role.py
@@ -40,21 +40,21 @@ async  def help(ctx):
 # admin help
 @bot.command()
 @commands.has_permissions(administrator = True)
-async  def ahelp(ctx):
-        emb = discord.Embed(title= "Админ команды:", colour= discord.Colour.orange())
-
-        await ctx.channel.purge(limit=1)
-
-        emb.add_field(name="Очистить чат", value="{}clear либо .clear (число - сколько удалить сообщений)".format(prefix),inline=False)
-        emb.add_field(name="Информация про пользователя", value="{}ainfo (ID пользователя) | Хеллпер".format(prefix),inline=False)
-        emb.add_field(name="Забанить", value="{}ban [ID пользователя] [причина бана]".format(prefix),inline=False)
-        emb.add_field(name="Разбанить", value="{}unban [ID пользователя] (в разработке)".format(prefix),inline=False)
-        emb.add_field(name="Кикинуть", value="{}kick [ID пользователя] [причина кика]".format(prefix),inline=False)
-        emb.add_field(name="Дать мут", value="{}mute [ID пользователя] [причина мута] | Хеллпер".format(prefix),inline=False)
-        emb.add_field(name="Снять мут", value="{}unmute [ID пользователя] | Хеллпер".format(prefix),inline=False)
-        emb.add_field(name="Админ команды:", value="{}ahelp".format(prefix),inline=False)
-
-        await ctx.send(embed=emb)
+async  def ahelp(ctx, user:discord.Member):
+	author=ctx.message.author
+    role_names=[role.name for role in author.roles]
+    emb = discord.Embed(title= "Админ команды:", colour= discord.Colour.orange())
+    await ctx.channel.purge(limit=1)
+    emb.add_field(name="Очистить чат", value="{}clear либо .clear (число - сколько удалить сообщений)".format(prefix),inline=False)
+    if "Хелпер" in role_names | administrator in author:
+        emb.add_field(name="Дать мут", value="{}mute [Упомянуть пользователя - '@'] [причина мута]".format(prefix),inline=False)
+    	emb.add_field(name="Снять мут", value="{}unmute [Упомянуть пользователя - '@']".format(prefix),inline=False)
+    	emb.add_field(name="Админ команды:", value="{}ahelp".format(prefix),inline=False)
+    emb.add_field(name="Информация про пользователя", value="{}ainfo (Упомянуть пользователя - '@')".format(prefix),inline=False)
+    emb.add_field(name="Забанить", value="{}ban [Упомянуть пользователя - '@'] [причина бана]".format(prefix),inline=False)
+    emb.add_field(name="Разбанить", value="{}unban [Упомянуть пользователя - '@'] (в разработке)".format(prefix),inline=False)
+    emb.add_field(name="Кикинуть", value="{}kick [Упомянуть пользователя - '@'] [причина кика]".format(prefix),inline=False)
+	await ctx.send(embed=emb)
 # clear
 @bot.command()
 @commands.has_permissions(administrator = True)
@@ -209,9 +209,9 @@ async def ainfo(ctx,user: discord.Member):
         emb.add_field(name="Аккаунт создан:", value=str(user.created_at)[:10])
         role=discord.utils.get(user.guild.roles, id=670630763716149248)
         if role in user.roles:
-            emb.add_field(name='Мут:', value='Да')
+            emb.add_field(name='Заглушка:', value='Есть')
         else:
-            emb.add_field(name='Мут:', value='Нет')
+            emb.add_field(name='Заглушка:', value='Нету')
         emb.set_thumbnail(url= str(user.avatar_url))
         emb.add_field(name='Играет в:',value=user.activity)
         emb.set_footer(text= 'Смотрит {}'.format(ctx.author.name), icon_url =ctx.author.avatar_url)
@@ -219,9 +219,7 @@ async def ainfo(ctx,user: discord.Member):
     else:
         author = ctx.message.author
         await ctx.send(f'{author.mention} У тебя недостаточно прав!')
-
         
-
 token = os.environ.get("TOKEN")
 bot.run(str(token))
 
